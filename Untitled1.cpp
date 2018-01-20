7b25cd93ea4aeffc8217c66a5d8bd04824919a41
ALI <alimhmmd434@gmail.com>
2018-01-20 17:22:43 +0700

    memasukkan variable helm,merk,dan kualitas.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -6,8 +6,17 @@ class helm{
 	public:
 		void harga(string price);
 		string kualitas, warna, model;
+
+};		
 		
 int main(){
+	helm NHK, MDS, KYT;
 	
+	NHK.kualitas = "Desain elegan dan murah ";
+	MDS.kualitas = " semua ada dan bermacam warna ";
+	KYT.kualitas = " murah dan higt performa ";
+	
+	cout << (Honda.kualitas);
+	NHK.harga(" standar ");
 }
 
